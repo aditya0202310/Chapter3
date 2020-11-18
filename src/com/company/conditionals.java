aa3dcf0a8aa3af5f7bec81d835ab2e497d3bde26
aditya0202310 <66565008+aditya0202310@users.noreply.github.com>
2020-11-17 20:48:50 -0500

    Conditional methods

--- src/com/company/conditionals.java
+++ src/com/company/conditionals.java
@@ -31,13 +31,28 @@ public class conditionals {
 //        System.out.println(negPos(-4, 5, true));
 //        System.out.println(negPos(4, 5, true));
 //        System.out.println(negPos(4, -5, true));
-        System.out.println(pickUpPhone(false, false, true));
-        System.out.println(pickUpPhone(true, true, true));
-        System.out.println(pickUpPhone(true, true, false));
-        System.out.println(pickUpPhone(false, true, false));
-        System.out.println(pickUpPhone(true, false, false));
-        System.out.println(pickUpPhone(false, false, false));
-        System.out.println(setAlarm(1, false));
+//        System.out.println(pickUpPhone(false, false, true));
+//        System.out.println(pickUpPhone(true, true, true));
+//        System.out.println(pickUpPhone(true, true, false));
+//        System.out.println(pickUpPhone(false, true, false));
+//        System.out.println(pickUpPhone(true, false, false));
+//        System.out.println(pickUpPhone(false, false, false));
+//        System.out.println(setAlarm(1, false));
+//        System.out.println(setAlarm(5, false));
+//        System.out.println(setAlarm (0, false));
+//        System.out.println(setAlarm (2, true));
+//        System.out.println(setAlarm (4, true));
+//        System.out.println(setAlarm (6, true));
+//        System.out.println(onesDigitSame(23, 19, 13));
+//        System.out.println(onesDigitSame(23, 19, 12));
+//        System.out.println(onesDigitSame(23, 19, 3));
+//        System.out.println(onesDigitSame(423, 13, 3));
+//        System.out.println(onesDigitSame(23, 29, 25));
+        System.out.println(blackjack(19, 21));
+        System.out.println(blackjack(21, 19));
+        System.out.println(blackjack(19, 22));
+        System.out.println(blackjack(8, 8));
+        System.out.println(blackjack(25, 24));
     }
     //Second method
 
@@ -59,12 +74,12 @@ public class conditionals {
         if (isAsleep == true) {
             return false;
         }
-        if (isMorning && isMom || isAsleep) {
+        if (isMorning && isMom) {
             return true;
         }
-        if (isMorning || isMom && isAsleep) {
+        if (isMorning && !isMom) {
             return false;
-        } else if (isMorning || isMom && isAsleep) {
+        } else if (!isMorning && isMom) {
             return true;
         } else
             return true;
@@ -72,11 +87,56 @@ public class conditionals {
 
     //Fourth Method
     public static String setAlarm(int dayOfWeek, boolean onVacation) {
-        if(dayOfWeek == 1 && dayOfWeek == 1  && onVacation == false){
-            String result = "7:00";
-            return result;
+        if (onVacation == false) {
+
+            if ((dayOfWeek >= 1 && dayOfWeek <= 5)) {
+                return "7:00";
+
+            } else {
+                return "10:00";
+            }
+
+        } else {
+            if (onVacation == true) {
+
+                if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+                    return "10:00";
+                } else {
+
+                }
+            }
+            return "off";
         }
-        else
-            return "10:00";
+    }
+
+    // fifth  method
+    public static boolean onesDigitSame(int a, int b, int c) {
+        if (a % 10 == c % 10 || a % 10 == b % 10) {
+            return true;
+        } else
+            return false;
+
+    }
+
+    // Sixth method
+
+    public static int blackjack(int x, int y) {
+        if ((x >= 0 && x <= 21) && (y >= 0 && y <= 21)) {
+            if (21 - x > 21 - y) {
+                return y;
+            } else {
+                return x;
+            }
+        }else if (y > 21) {
+            if(x <= 21){
+                return x;
+            }else
+                return 0;
+
+        } else
+            return 0;
     }
 }
+
+
+
